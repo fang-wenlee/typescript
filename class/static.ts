0b3992061ef7231f820ce2dfd7305b6bcd7e1db6
Fang-Wen Lee <fangwenlee@hotmail.com>
2021-10-18 16:59:09 -0700

    typescript for extend Buildin Class Array

--- class/static.ts
+++ class/static.ts
@@ -19,6 +19,7 @@ class Triple {
     }
   }
   
+
   console.log(Triple.description);            // 'I triple any number you provide'
   console.log(Triple.calculate());            // 3
   console.log(Triple.calculate(6));           //18
@@ -28,3 +29,62 @@ class Triple {
 console.log(SquaredTriple.calculate(3));    // 81 (not affected by parent's instantiation)
 console.log(SquaredTriple.description);     // 'I square the triple of any number you provide'
 //console.log(SquaredTriple.longDescription); // undefined
+console.log('+++++++++++++++++++++++++++++++++++++++++++++++');
+console.log('+++++++++++++++++++++++++++++++++++++++++++++++');
+//=============================================================
+//========================================================
+class Meg{
+      title: string;
+      message: string;
+      _isSent: boolean= false;
+
+      get isSent():boolean{
+           return this._isSent
+      }
+      set isSent(vale:boolean){
+          this._isSent = vale;
+      }
+      constructor(title:string, message: string){
+           this.title = title;
+           this.message= message;
+      }
+      get briefMeg():string{
+           return this.message.slice(0, 10).concat("...")
+      }
+
+}
+const greeting = new Meg("Moday working Status", "What is the progree for ticket")
+
+// console.log(greeting.briefMeg)
+// console.log ( greeting.isSent)
+// console.log ( greeting.isSent = true)
+// console.log ( greeting.isSent)
+
+//class Messages extends Array<Meg> {
+   
+    // public getValidMessags():Meg[]{
+    //     return this.filter(value=> value.message.trim().length > 0 )
+    // }
+    // public static getValidMessags(messages: Meg[]):Meg[]{
+    //     console.log(messages)
+    //       return messages.filter(value=> value.message.length > 0 );
+    //  }
+//}
+
+
+//console.log ( Messages.getValidMessags( [] ) )
+ 
+// const newMeg = new Messages<Meg>("Good mornig");
+//  console.log(newMeg)
+// console.log(newMeg.getValidMessags<Meg>() );
+
+class MyArray<T> extends Array<T> {
+    remove: (elem: T) => Array<T> = function(elem: T) {
+        return this.filter(e => e !== elem);
+    }
+}
+let myArr = new MyArray<string>("Good", "Bad", "some");
+console.log(myArr)
+// console.log ( myArr.remove("some"))
+
+
--- dist/static.js
+++ dist/static.js
@@ -44,3 +44,64 @@ var tp = new Triple(); //cant acess either protected propter or method
 console.log(SquaredTriple.calculate(3)); // 81 (not affected by parent's instantiation)
 console.log(SquaredTriple.description); // 'I square the triple of any number you provide'
 //console.log(SquaredTriple.longDescription); // undefined
+console.log('+++++++++++++++++++++++++++++++++++++++++++++++');
+console.log('+++++++++++++++++++++++++++++++++++++++++++++++');
+//=============================================================
+//========================================================
+var Meg = /** @class */ (function () {
+    function Meg(title, message) {
+        this._isSent = false;
+        this.title = title;
+        this.message = message;
+    }
+    Object.defineProperty(Meg.prototype, "isSent", {
+        get: function () {
+            return this._isSent;
+        },
+        set: function (vale) {
+            this._isSent = vale;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Meg.prototype, "briefMeg", {
+        get: function () {
+            return this.message.slice(0, 10).concat("...");
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Meg;
+}());
+var greeting = new Meg("Moday working Status", "What is the progree for ticket");
+// console.log(greeting.briefMeg)
+// console.log ( greeting.isSent)
+// console.log ( greeting.isSent = true)
+// console.log ( greeting.isSent)
+//class Messages extends Array<Meg> {
+// public getValidMessags():Meg[]{
+//     return this.filter(value=> value.message.trim().length > 0 )
+// }
+// public static getValidMessags(messages: Meg[]):Meg[]{
+//     console.log(messages)
+//       return messages.filter(value=> value.message.length > 0 );
+//  }
+//}
+//console.log ( Messages.getValidMessags( [] ) )
+// const newMeg = new Messages<Meg>("Good mornig");
+//  console.log(newMeg)
+// console.log(newMeg.getValidMessags<Meg>() );
+var MyArray = /** @class */ (function (_super) {
+    __extends(MyArray, _super);
+    function MyArray() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.remove = function (elem) {
+            return this.filter(function (e) { return e !== elem; });
+        };
+        return _this;
+    }
+    return MyArray;
+}(Array));
+var myArr = new MyArray("Good", "Bad", "some");
+console.log(myArr);
+// console.log ( myArr.remove("some"))
